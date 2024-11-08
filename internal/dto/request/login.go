62cc18117f8b8cf6b44125bf7ba918bbbba08a2f
Zero <mengxiangyu996@qq.com>
2024-11-08 17:19:15 +0800

    chore: 去除form标签

--- internal/dto/request/login.go
+++ internal/dto/request/login.go
@@ -2,13 +2,13 @@ package request
 
 // 验证码
 type Captcha struct {
-	Id    string `json:"id" form:"id"`
-	Value string `json:"value" form:"value"`
+	Id    string `json:"id"`
+	Value string `json:"value"`
 }
 
 // 登录
 type LoginReq struct {
-	Username string  `json:"username" form:"username"`
-	Password string  `json:"password" form:"password"`
-	Captcha  Captcha `json:"captcha" form:"captcha"`
+	Username string  `json:"username"`
+	Password string  `json:"password"`
+	Captcha  Captcha `json:"captcha"`
 }
