d3f9a83a2a8659aa044011d7ad1213a4d5a8c93b
tangtanglove <dai_hang_love@126.com>
2025-01-03 17:57:09 +0800

    chore: wip

--- internal/service/item.go
+++ internal/service/item.go
@@ -5,3 +5,15 @@ type ItemService struct{}
 func NewItemService() *ItemService {
 	return &ItemService{}
 }
+
+func (p *ItemService) StoreItemAttr() {
+}
+
+func (p *ItemService) UpdateItemAttr() {
+}
+
+func (p *ItemService) StoreItemAttrValue() {
+}
+
+func (p *ItemService) UpdateItemAttrValue() {
+}
