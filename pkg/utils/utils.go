6638e3916a80fc4a8f86253d91459b1732dc7628
Zero <mengxiangyu996@qq.com>
2025-01-06 10:44:39 +0800

    feat: 完善工具类

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -81,3 +81,50 @@ func CheckRegex(expr, content string) bool {
 	}
 	return r.MatchString(content)
 }
+
+// 比较工具
+// 检查元素item是否存在于切片slice中
+// 如果存在，返回true；如果不存在，返回false
+func Contains[T comparable](slice []T, item T) bool {
+
+	for _, value := range slice {
+		if value == item {
+			return true
+		}
+	}
+
+	return false
+}
+
+// 过滤器
+// 条件函数返回true，元素会被包含在结果中
+func Filter[T interface{}](slice []T, condition func(T) bool) (result []T) {
+
+	for _, value := range slice {
+		if condition(value) {
+			result = append(result, value)
+		}
+	}
+
+	return result
+}
+
+// 脱敏工具
+func Desensitize(content string, start, end int) string {
+
+	if start < 0 || end < 0 || start > end {
+		return content
+	}
+
+	var contentRune []rune
+
+	for key, value := range content {
+		if key >= start && key <= end {
+			contentRune = append(contentRune, '*')
+		} else {
+			contentRune = append(contentRune, value)
+		}
+	}
+
+	return string(contentRune)
+}
