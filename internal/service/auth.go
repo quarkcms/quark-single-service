bc04d2f5e1c650a581c00907aaeb5c8f960e60cc
tangtanglove <dai_hang_love@126.com>
2025-01-08 15:27:09 +0800

    chore: wip

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"errors"
+
 	"github.com/quarkcloudio/quark-go/v3"
 	"github.com/quarkcloudio/quark-go/v3/dto"
+	"github.com/quarkcloudio/quark-go/v3/model"
 )
 
 type AuthService struct {
@@ -13,22 +16,22 @@ func NewAuthService(ctx *quark.Context) *AuthService {
 	return &AuthService{ctx}
 }
 
-// 获取当前登录用户JWT信息
-func (p *AuthService) GetUserClaims() (userClaims dto.UserClaims, err error) {
-	userClaims = dto.UserClaims{}
-	err = p.ctx.JwtAuthUser(&userClaims)
-	return userClaims, err
-}
-
 // 获取当前登录用户信息
-func (p *AuthService) GetUser() (userId int, err error) {
+func (p *AuthService) GetUser() (user model.User, err error) {
 	userClaims := dto.UserClaims{}
 	err = p.ctx.JwtAuthUser(&userClaims)
-	return userClaims.Id, err
+	if err != nil {
+		return user, err
+	}
+	userInfo, err := NewUserService().GetInfoById(userClaims.Id)
+	if user.Status == 0 {
+		return user, errors.New("用户被禁用")
+	}
+	return userInfo, err
 }
 
 // 获取当前登录用户ID
 func (p *AuthService) GetUid() (userId int, err error) {
-	userClaims, err := p.GetUserClaims()
+	userClaims, err := p.GetUser()
 	return userClaims.Id, err
 }
