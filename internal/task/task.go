2bb7955cead188d394b731be7c2a8f7940540a25
Zero <mengxiangyu996@qq.com>
2025-01-13 09:29:01 +0800

    chore: 增加定时启动条件

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,6 +1,7 @@
 package task
 
 import (
+	"github.com/quarkcloudio/quark-smart/v2/config"
 	"github.com/quarkcloudio/quark-smart/v2/pkg/scheduler"
 )
 
@@ -9,8 +10,10 @@ func RegisterTask() {
 	// 初始化调度器
 	s := scheduler.NewScheduler()
 
-	// 账单任务
-	RunBillTask(s.Cron)
+	if config.App.Pro {
+		// 账单任务
+		RunBillTask(s.Cron)
+	}
 
 	// 启动调度器
 	s.Start()
